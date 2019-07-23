0621a54e7b6f2bd8eacffc92b9e86441773136ea
xiaohu <1322081197@qq.com>
2019-07-23 14:58:58 +0800

    加入shiro

--- src/main/java/com/xiaohu/demo/common/shiro/ShiroConfig.java
+++ src/main/java/com/xiaohu/demo/common/shiro/ShiroConfig.java
@@ -1,11 +1,9 @@
 package com.xiaohu.demo.common.shiro;
 
-import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.util.LinkedHashMap;
